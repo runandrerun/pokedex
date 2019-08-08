5104ba5974c9821ba20f382ee1bfb2c37f9ae665
Santiago <andre.santiago@area23hc.com>
2019-08-08 15:33:56 -0400

    React Native setup and structural additions

---
+++ app/Router.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { Dimensions, Platform } from 'react-native';
+import { StackNavigator, TabNavigator } from 'react-navigation';
+import { Icon } from 'react-native-elements';
+
+import Pokemon from './screens/Bookcase';
+import Explore from './screens/Explore';
+import AddPokemon from './screens/AddBook';
+import Lists from './screens/Lists';
+import Profile from './screens/Profile';
+
+let screen = Dimensions.get('window');
+
+export const Tabs = TabNavigator({
+  'Pokemon': {
+    screen: Pokemon,
+    navigationOptions: {
+      tabBarLabel: 'Pokemon',
+      tabBarIcon: ({ tintColor }) => <Icon name="open-book" type="entypo" size={28} color={tintColor} />
+    },
+  },
+  'Explore': {
+    screen: Explore,
+    navigationOptions: {
+      tabBarLabel: 'Explore',
+      tabBarIcon: ({ tintColor }) => <Icon name="ios-map-outline" type="ionicon" size={28} color={tintColor} />
+    },
+  },
+  'Add Pokemon': {
+    screen: AddPokemon,
+    navigationOptions: {
+      tabBarLabel: 'Add Pokemon',
+      tabBarIcon: ({ tintColor }) => <Icon name="ios-add-circle-outline" type="ionicon" size={28} color={tintColor} />
+    },
+  },
+  'Lists': {
+    screen: PokemonListStack,
+    navigationOptions: {
+      tabBarLabel: 'Lists',
+      tabBarIcon: ({ tintColor }) => <Icon name="list" type="entypo" size={28} color={tintColor} />
+    },
+  },
+  'My Profile': {
+    screen: Profile,
+    navigationOptions: {
+      tabBarLabel: 'Profile',
+      tabBarIcon: ({ tintColor }) => <Icon name="ios-person-outline" type="ionicon" size={28} color={tintColor} />
+    },
+  },
+});
+
+export const createRootNavigator = () => {
+  return StackNavigator(
+    {
+      Tabs: {
+        screen: Tabs,
+        navigationOptions: {
+          gesturesEnabled: false
+        }
+      }
+    },
+  );
+};
